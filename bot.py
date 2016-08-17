22cc1b39d924ff721040247c8446a01f0f55b6f5
bis <bis83gb@gmail.com>
2016-08-18 06:56:47 +0900

    add warning if SLACKBOT_URL is not set.

--- bot.py
+++ bot.py
@@ -4,7 +4,15 @@
 import slackweb
 import os
 
-if __name__ == '__main__':
-    slack = slackweb.Slack(url=os.getenv("SLACKBOT_URL"))
+url = os.getenv("SLACKBOT_URL")
+
+def main():
+    if not url:
+        print "Warn: SLACKBOT_URL is not set."
+        return
+    slack = slackweb.Slack(url=url)
     slack.notify(text="from RaspberryPi.")
+
+if __name__ == '__main__':
+    main()
 
